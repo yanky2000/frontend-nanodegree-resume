31bff8220351545d1e0df155c3f809424db1e805
VY <khi.chan.yang@gmail.com>
2015-10-28 21:55:25 +0300

    Add work and education, updated notes on JS objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,5 +11,47 @@ var role = "Web developer";
 var formattedRole = HTMLheaderRole.replace("%data%",role);
 
 
-$("#header").prepend(formattedName);
-$("#header").append(formattedRole);
+// $("#header").prepend(formattedName);
+// $("#header").append(formattedRole);
+
+var formattedEmail = HTMLemail.replace("%data%","yanky2000@yahoo.com")
+
+var bio = {
+	"name":HTMLheaderName.replace("%data%","Victor Yang"),
+	"role":HTMLheaderRole.replace("%data%","Web Developer"),
+	"yearOfBirth": 1976,
+	"contact":{
+		"mobile": HTMLmobile.replace("%data%","+79150610055"),
+		"email": HTMLemail.replace("%data%","yanky2000@yahoo.com"),
+	},
+	"bioPic":HTMLbioPic.replace("%data%","images/my_photo.png"),
+	"skills":HTMLskills.replace("%data%",["JS","Web development","russian, korean languages"]),
+	"welcomeMsg":HTMLwelcomeMsg.replace("%data%","Welcome to my page!")
+};
+
+$("#header").prepend(bio.name,bio.role);
+$("#header").append(bio.bioPic, bio.skills, bio.contact.mobile, bio.contact.email, bio.welcomeMsg);
+
+var work = {};
+	work.city = HTMLworkLocation.replace("%data%","Moscow");
+	work.title = HTMLworkTitle.replace("%data%","self-employed");
+	work.dates = HTMLworkDates.replace("%data%","2013");
+
+// console.log(work.title);
+// console.log(work.dates);
+// console.log(work.city);
+// console.log(bio.age+1);
+
+var education = {}
+	education["school"] = HTMLschoolName.replace("%data%","Kangwon National Universtiy");
+	education["dates"] = HTMLschoolDates.replace("%data%","1999-2001");
+	education["major"] = HTMLschoolMajor.replace("%data%","Marketing");
+	education["location"] = HTMLschoolLocation.replace("%data%", "Chounchon, S.Korea");
+
+// console.log(education.school);
+// console.log(education.dates);
+// console.log(education.major);
+// console.log(education.location);
+
+$("#main").append(work["title"]);
+$("#main").append(education.school);
--- notes.js
+++ notes.js
@@ -7,6 +7,7 @@ array.push(...); // Append value to Array to last position.
 
 //[.split] splits the string into Array. Splitted parts of the String become Array's values.
 //Limit is a number of times .split actually makes the split. i.e. number of values
+//Separator can be Regular Expression (see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions)
 var splittedArray = str.split(separator,[limit]);
 
 //Makes all characters in String  to be UpperCase/LowerCase
@@ -14,3 +15,18 @@ string.toUpperCase(); string.toLowerCase();
 
 //Joining elements of Array in a single string, with an optional Separator between them
 Array.join(separator);
+
+
+// JS Day 2. (28.10.15)
+// Objects
+
+var bio = {
+	"name": "victor";
+};
+bio.age = 39; //Defining property using DOT (.) notation
+bio["sex"] = "male"; // Deifning object using [] notation
+
+//[] notation is used if property use special characters (!, {} etc.),
+// it is also used in formulas ex:(console.log(bio.age +1)
+$("#header").append(bio.name); //Appending bio.name to header into index.html
+
